b7cae057cce2e43db38f713fb3db6c020c6ca250
Parsa Soleimani <61251770+parsoli83@users.noreply.github.com>
2020-09-07 18:31:35 +0430

    Update pypy.py

--- pypy.py
+++ pypy.py
@@ -1,24 +1,30 @@
 from subprocess import check_output
 from os import pipe,write,close
-
 def pypy(path,args=False):
+    def to_string(l):
+        s=""
+        for i in l:
+            s+=i+"\n"
+        return s
+    to_list=lambda s:s.split("\n")[:-1]
     if args==False:
         data=None
     else:
         data, temp = pipe() 
-        write(temp, bytes(args, "utf-8")); 
+        write(temp, bytes(to_string(args), "utf-8")); 
         close(temp) 
-    s = check_output("pypy3 "+path, stdin = data, shell = True) 
-    return s.decode("utf-8")
+    s = check_output("cd "+path+";cargo run", stdin = data, shell = True) 
+    return to_list(s.decode("utf-8"))
 
 #HOW TO USE:
 #the first argument is the path
-#the second is arguments seperated by \n
+#the second is the list of arguments
 """
-print(pypy("/home/parsa/AAA_RUST/hello_world.py","a\nb\n12"))
+print(pypy("/home/parsa/AAA_RUST/hello_world.py",["a","b"]))
 """
 #arguments can be ignored if there is no
 """
 print(pypy("/home/parsa/AAA_RUST/hello_world.py"))
 """
-#returns a string seperated by \n 
+#returns a list of strings
+
